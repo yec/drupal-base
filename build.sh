4b881964dd2b03b653f8f4d80f1de94c86d53f00
Ye Chuah <yechuah@gmail.com>
2013-02-02 07:11:06 -0500

    remove drush commands from build

--- build.sh
+++ build.sh
@@ -2,15 +2,4 @@ site=base
 drush make drupal-base.make --contrib-destination .
 drush si --account-mail=yechuah@gmail.com --account-name=admin --account-pass=admin --db-url=mysql://root@localhost/$site -y
 
-drush dis toolbar            -y
-drush dis dblog              -y
-
-drush en  views_ui           -y
-drush en  features           -y
-drush en  admin_menu_toolbar -y
-drush en  strongarm          -y
-drush en  views_ifempty      -y
-drush en  devel_generate     -y
-
 chown -R apache:apache sites/default/files
-drush fra -y
